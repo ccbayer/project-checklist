3cf0a4a1081ba1bc00738bae7ba3ce5e0ba3c0d6
ccbayer <ccbayer@gmail.com>
2016-09-21 14:58:40 -0700

    CCB update port to support dynamic heroku port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// process.env.PORT needed as heroku dynamically assigns port.
+// http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port ' + process.env.PORT + '!');
 });
