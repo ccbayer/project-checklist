04d5b2cd3c38ff66fdb85e2986807fb1b69d86cd
ccbayer <ccbayer@gmail.com>
2016-09-27 15:45:43 -0700

    CB-authentication first step

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project-checklist
 project-checklist
+
+# Run This Locally
+1. install dependencies using `npm install`
+2. start express by running `node index.js`
+3. open browser; go to `localhost:5000`
--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
+passport.use(new GoogleStrategy({
+    clientID: "243948653223-ogu029nca575dp4k4rvbagficd09lu8k.apps.googleusercontent.com",
+    clientSecret: "6eQWjsnnTRGPh_FZLRQ8ukCd",
+    callbackURL: "https://epsilon-project-checklist.herokuapp.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,7 +28,16 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+// set up routes for authentication
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
